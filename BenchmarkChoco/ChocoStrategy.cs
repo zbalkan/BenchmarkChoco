d898ebf0b21b8cb02ec659ec893100d81b9bc213
Zafer Balkan <zafer@zaferbalkan.com>
2021-09-27 13:57:39 +0300

    Added base setup for BenchmarkDotNet

---
+++ BenchmarkChoco/ChocoStrategy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+
+namespace BenchmarkChoco
+{
+    public class ChocoStrategy
+    {
+        [Benchmark]
+
+        public void ParsingExecutableOutputStrategy()
+        {
+
+        }
+        [Benchmark]
+
+        public void UsingLibraryStrategy()
+        {
+
+        }
+    }
+}
--- BenchmarkChoco/Program.cs
+++ BenchmarkChoco/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BenchmarkDotNet.Running;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,8 @@ namespace BenchmarkChoco
     {
         public static void Main(string[] args)
         {
+            var summary = BenchmarkRunner.Run<ChocoStrategy>();
+
         }
     }
 }
