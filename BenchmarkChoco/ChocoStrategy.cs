3a4ff5e9cac0324bb45c00fe38caa700bd2cf9f4
Zafer Balkan <zafer@zaferbalkan.com>
2021-09-28 20:19:02 +0300

    Set the count as 20 as it's a just enough to test and not too much to wait for the results

--- BenchmarkChoco/ChocoStrategy.cs
+++ BenchmarkChoco/ChocoStrategy.cs
@@ -4,7 +4,7 @@ using BenchmarkDotNet.Engines;
 
 namespace BenchmarkChoco
 {
-    [SimpleJob(RunStrategy.Monitoring, launchCount: 1, warmupCount: 1, targetCount: 5)]
+    [SimpleJob(RunStrategy.Monitoring, launchCount: 1, warmupCount: 1, targetCount: 20)]
     public class ChocoStrategy
     {
         [Benchmark]
