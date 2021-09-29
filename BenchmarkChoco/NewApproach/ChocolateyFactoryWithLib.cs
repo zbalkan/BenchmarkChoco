6063df08ad1985d269cd229d5b9c756447d6abc3
Zafer Balkan <zafer@zaferbalkan.com>
2021-09-30 00:32:55 +0300

    A major rewrite of new ChocolateyFactory

--- BenchmarkChoco/NewApproach/ChocolateyFactoryWithLib.cs
+++ BenchmarkChoco/NewApproach/ChocolateyFactoryWithLib.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
-using System.Text;
+using System.IO;
+using System.Linq;
 using chocolatey;
 using chocolatey.infrastructure.app.domain;
 using chocolatey.infrastructure.results;
@@ -11,59 +12,29 @@ namespace BenchmarkChoco
 {
     public sealed class ChocolateyFactoryWithLib
     {
-        private static bool? _chocoIsAvailable;
-
         private readonly GetChocolatey _choco;
 
-        // KEEP: to check if Choco is available
-        private static bool ChocoIsAvailable
-        {
-            get
-            {
-                if (!_chocoIsAvailable.HasValue)
-                {
-                    _chocoIsAvailable = false;
-                    GetChocoInfo();
-                }
-                return _chocoIsAvailable.Value;
-            }
-        }
+        private static bool? chocoIsAvailable = null;
 
-        public static string ChocoFullFilename { get; private set; }
+        private static string chocoFullFilename;
 
         public ChocolateyFactoryWithLib()
         {
-            _choco = Lets.GetChocolatey();
-        }
-
-        // KEEP: to check if Choco is available, no need for location
-        private static void GetChocoInfo()
-        {
-            try
+            GatherInfo();
+            if (chocoIsAvailable.Value)
             {
-                ChocoFullFilename = PathTools.GetFullPathOfExecutable("choco.exe");
-                if (string.IsNullOrEmpty(ChocoFullFilename)) return;
-
-                var result = StartProcessAndReadOutput(ChocoFullFilename, string.Empty);
-                if (result.StartsWith("Chocolatey", StringComparison.Ordinal))
-                {
-                    _chocoIsAvailable = true;
-                }
-            }
-            catch (SystemException ex)
-            {
-                Console.WriteLine(ex);
+                _choco = Lets.GetChocolatey();
             }
         }
 
-        // KEEP: One and only public method. Rewrite almost all. 
         [SuppressMessage("Redundancy", "RCS1163:Unused parameter.", Justification = "Callback is used with the result, not in the method itself.")]
         [SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Callback is used with the result, not in the method itself.")]
         [SuppressMessage("CodeQuality", "IDE0079:Remove unnecessary suppression", Justification = "Just stop nagging.")]
         public IList<ApplicationUninstallerEntry> GetUninstallerEntries(ListGenerationProgress.ListGenerationCallback progressCallback = null)
         {
             var results = new List<ApplicationUninstallerEntry>();
-            if (!ChocoIsAvailable) return results;
+            if (!chocoIsAvailable.Value) return results;
+
             _choco.Set(
                 config =>
                 {
@@ -78,29 +49,26 @@ namespace BenchmarkChoco
             foreach (var package in packages)
             {
                 sw.Restart();
-                var entry = new ApplicationUninstallerEntry
+                var entry = new ApplicationUninstallerEntry();
+                entry.DisplayName = SanitizeString(package.Package.Title);
+                entry.DisplayVersion = ApplicationEntryTools.CleanupDisplayVersion(package.Version);
+                entry.Comment = SanitizeString(package.Package.Summary ?? package.Package.Description.Split('\n')[0] ?? package.Package.Tags);
+                entry.AboutUrl = package.Package.DocsUrl?.AbsoluteUri ?? package.Package.ProjectUrl?.AbsoluteUri;
+                entry.InstallLocation = package.InstallLocation;
+                entry.DisplayIcon = string.Empty;
+                entry.EstimatedSize = FileSize.Empty;
+                entry.IconBitmap = null;
+                entry.InstallDate = DateTime.MinValue;
+                entry.RatingId = $"Choco {package.Package.Id}";
+                entry.UninstallerKind = UninstallerType.Chocolatey;
+
+                // A special occasion that needs to be handled.
+                if (package.Package.Title.Equals("Chocolatey"))
                 {
-                    // Info in the package result
-                    DisplayVersion = ApplicationEntryTools.CleanupDisplayVersion(package.Version),
-                    InstallLocation = package.InstallLocation,
-                    InstallSource = package.Source,
+                    entry.InstallLocation = Environment.GetEnvironmentVariable("ChocolateyInstall");
+                }
 
-                    // Info in the nested IPackage
-                    DisplayName = package.Package.Title,
-                    AboutUrl = package.Package.ProjectUrl?.AbsolutePath ?? package.Package.DocsUrl?.AbsolutePath,
-                    Comment = package.Package.Description ?? package.Package.Tags,
-
-                    // Unknown parts
-                    DisplayIcon = string.Empty,
-                    EstimatedSize = FileSize.Empty,
-                    IconBitmap = null,
-                    InstallDate = DateTime.Today,
-                    Is64Bit = MachineType.X64,
-                    RatingId = $"Choco {package.Name}",
-                    UninstallerKind = UninstallerType.Chocolatey
-                };
-
-                var psc = new ProcessStartCommand(ChocoFullFilename, $"uninstall {package.Name} -y -r");
+                var psc = new ProcessStartCommand(chocoFullFilename, $"uninstall {package.Package.Id} -y -r");
                 entry.UninstallString = psc.ToString();
 
                 // Prevent chocolatey from trying to run the original uninstaller (it's deleted by now), only remove the package
@@ -111,27 +79,57 @@ namespace BenchmarkChoco
                 entry.AdditionalJunk.Add(junk);
 
                 results.Add(entry);
-                Console.WriteLine($"[Performance] Mapping PackageResult to Application entry for {package.Name} took {sw.ElapsedMilliseconds}ms");
+                Console.WriteLine($"[Performance] Mapping PackageResult to Application entry for {package.Package.Id} took {sw.ElapsedMilliseconds}ms");
             }
             return results;
         }
 
-        private static string StartProcessAndReadOutput(string filename, string args)
+        private void GatherInfo()
         {
-            using (var process = Process.Start(new ProcessStartInfo(filename, args)
+            var chocoPath = string.Empty;
+            if (string.IsNullOrWhiteSpace(chocoFullFilename))
             {
-                UseShellExecute = false,
-                RedirectStandardOutput = true,
-                RedirectStandardError = false,
-                CreateNoWindow = true,
-                StandardOutputEncoding = Encoding.Default
-            }))
-            {
-                var sw = Stopwatch.StartNew();
-                var output = process?.StandardOutput.ReadToEnd();
-                Console.WriteLine($"[Performance] Running command {filename} {args} took {sw.ElapsedMilliseconds}ms");
-                return output;
+                var pathVar = Environment.GetEnvironmentVariable("PATH");
+                if (!string.IsNullOrWhiteSpace(pathVar))
+                {
+                    chocoPath =
+                        pathVar.Split(';')
+                            .SingleOrDefault(
+                                path => path.IndexOf("Chocolatey", StringComparison.OrdinalIgnoreCase) > -1);
+                }
             }
+
+            if (string.IsNullOrWhiteSpace(chocoPath))
+            {
+                chocoPath = string.Empty;
+            }
+            var exePath = Path.Combine(chocoPath, "choco.exe");
+            if (File.Exists(exePath))
+            {
+                chocoIsAvailable = true;
+                chocoFullFilename = exePath;
+            }
+        }
+
+        // Chocolatey CLI probably handles this before printing, that's why we do not need this code while parsing
+        private string SanitizeString(string text)
+        {
+            if (text.IndexOf('\u2013') > -1) text = text.Replace('\u2013', '-'); // en dash
+            if (text.IndexOf('\u2014') > -1) text = text.Replace('\u2014', '-'); // em dash
+            if (text.IndexOf('\u2015') > -1) text = text.Replace('\u2015', '-'); // horizontal bar
+            if (text.IndexOf('\u2017') > -1) text = text.Replace('\u2017', '_'); // double low line
+            if (text.IndexOf('\u2018') > -1) text = text.Replace('\u2018', '\''); // left single quotation mark
+            if (text.IndexOf('\u2019') > -1) text = text.Replace('\u2019', '\''); // right single quotation mark
+            if (text.IndexOf('\u201a') > -1) text = text.Replace('\u201a', ','); // single low-9 quotation mark
+            if (text.IndexOf('\u201b') > -1) text = text.Replace('\u201b', '\''); // single high-reversed-9 quotation mark
+            if (text.IndexOf('\u201c') > -1) text = text.Replace('\u201c', '\"'); // left double quotation mark
+            if (text.IndexOf('\u201d') > -1) text = text.Replace('\u201d', '\"'); // right double quotation mark
+            if (text.IndexOf('\u201e') > -1) text = text.Replace('\u201e', '\"'); // double low-9 quotation mark
+            if (text.IndexOf('\u2026') > -1) text = text.Replace("\u2026", "..."); // horizontal ellipsis
+            if (text.IndexOf('\u2032') > -1) text = text.Replace('\u2032', '\''); // prime
+            if (text.IndexOf('\u2033') > -1) text = text.Replace('\u2033', '\"'); // double prime
+            if (text.IndexOf('\u00AE') > -1) text = text.Replace('\u00AE', '©'); // register mark
+            return text;
         }
     }
 }
