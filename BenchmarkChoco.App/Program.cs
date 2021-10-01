03048da292f01ffcb51e3c549838b6ca3a2df0aa
Zafer Balkan <zafer@zaferbalkan.com>
2021-10-01 10:50:38 +0300

    Added a console application project as a custom testbed

---
+++ BenchmarkChoco.App/Program.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace BenchmarkChoco.App
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var expectedEntries = new ChocolateyFactory().GetUninstallerEntries(null);
+            var actualEntries = new ChocolateyFactoryWithLib().GetUninstallerEntries(null);
+
+            for (var i = 0; i < expectedEntries.Count; i++)
+            {
+                var expectedJson = JsonConvert.SerializeObject(expectedEntries[i], Formatting.Indented);
+                var actualJson = JsonConvert.SerializeObject(actualEntries[i], Formatting.Indented);
+                var correct = expectedJson.Equals(actualJson, StringComparison.OrdinalIgnoreCase);
+                Console.WriteLine($"{i}: {correct}");
+                if (!correct)
+                {
+                    Console.WriteLine("Expected:");
+                    Console.WriteLine(expectedJson);
+                    Console.WriteLine("Actual:");
+                    Console.WriteLine(actualJson);
+                }
+            }
+        }
+    }
+}
